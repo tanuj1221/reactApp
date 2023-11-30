500a6e415a13fa8ef6fdc260b4df6f14fe719449
tanuj1221 <tcchavan999@gmail.com>
2023-11-30 19:27:19 +0530

    5

--- server.js
+++ server.js
@@ -866,8 +866,6 @@ function createToken(payload) {
 
 
 
-const port = 5000;
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
